810231ede9bfa59cfc897a970641e50ad6401be2
Josh Brito <jbrito6492@gmail.com>
2022-07-19 22:16:15 -0700

    B - Added support for swagger

---
+++ TodoApi.Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var todos = new List<Todo>
+            {
+                new Todo { Id = 1, Name = "Laundry", IsComplete = false }
+            };
+            return Ok(todos);
+        }
+    }
+}
