13760d4580d42056bd4559ada1c64486ba9e7cde
Josh Brito <jbrito6492@gmail.com>
2022-07-23 18:39:19 -0700

    Behavioral - added Controllers and Models directories

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace TodoApi.TodoApi.Controllers
+namespace TodoApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
