bdc1b2ca5a29ba9350c6a3dbd23a1fe0139889ab
Josh Brito <jbrito6492@gmail.com>
2022-07-24 15:57:18 -0700

    Behavioral - finished Todo Controller

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
@@ -11,14 +12,61 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        private static List<Todo> _todos = new List<Todo>
         {
-            var todos = new List<Todo>
+            new Todo { Id = 1, Name = "Todo 1", IsComplete = false }
+        };
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<List<Todo>>> Get()
+        {
+            return Ok(_todos);
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<ActionResult<Todo>> Get(int id)
+        {
+            return _todos.FirstOrDefault(t => t.Id == id);
+        }
+
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<Todo>> Post(Todo todo)
+        {
+            _todos.Add(todo);
+            return CreatedAtAction("Get", new { id = todo.Id }, todo);
+        }
+
+
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Todo>> Put(int id, Todo todo)
+        {
+            var todoToUpdate = _todos.FirstOrDefault(t => t.Id == id);
+            if (todoToUpdate == null)
             {
-                new Todo { Id = 1, Name = "Laundry", IsComplete = false }
-            };
-            return Ok(todos);
+                return NotFound();
+            }
+
+            todoToUpdate.Name = todo.Name;
+            todoToUpdate.IsComplete = todo.IsComplete;
+            return NoContent();
+        }
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Todo>> Delete(int id)
+        {
+            var todoToDelete = _todos.FirstOrDefault(t => t.Id == id);
+            if (todoToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _todos.Remove(todoToDelete);
+            return NoContent();
         }
     }
 }
--- Models/Todo.cs
+++ Models/Todo.cs
@@ -5,9 +5,4 @@ public class Todo
     public int Id { get; set; }
     public string Name { get; set; }
     public bool IsComplete { get; set; }
-
-    public Todo()
-    {
-        
-    }
 }
