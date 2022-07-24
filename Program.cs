9d8ab96bcbfc2d6964dd6ffd27cf15b46fae856d
Josh Brito <jbrito6492@gmail.com>
2022-07-24 16:41:35 -0700

    Behavioral - Added sql server and entity framwork support

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
+global using TodoApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
